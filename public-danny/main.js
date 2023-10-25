eb17274210fae3a62d5b46f5231081302ba3064c
Danny Ryan <dannykryan@gmail.com>
2023-10-25 23:30:54 +0100

    updated form styling and link behaviour

--- public-danny/main.js
+++ public-danny/main.js
@@ -1,10 +1,14 @@
-const file = document.querySelector(".file")
-const form = document.getElementById("newRes")
-const main = document.getElementById("main")
+const resourceBtn = document.querySelector(".submit-resource-button");
+const form = document.getElementById("newRes");
+const main = document.getElementById("main");
+const bodyLinks = document.getElementsByClassName("body-link");
 
-function files(){
-    file.classList.toggle("rotate")
-    form.classList.toggle("formShow")
-    main.classList.toggle("fade")
+function files() {
+  resourceBtn.classList.toggle("rotate-and-hide");
+  form.classList.toggle("formShow");
+  main.classList.toggle("fade");
+
+  for (let i = 0; i < bodyLinks.length; i++) {
+    bodyLinks[i].classList.toggle("inactive-link");
+  }
 }
-
