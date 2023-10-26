0817455679f11a8d9adebc90fb48579521ecfdd0
MartiZu <martina.zurli@gmail.com>
2023-10-26 20:44:11 +0100

    testing done!

--- tests/buddy.test.js
+++ tests/buddy.test.js
@@ -4,14 +4,15 @@ import { test, expect } from "playwright/test";
 test("Click add new", async ({ page }) => {
   await page.goto("http://127.0.0.1:5501/public-danny/login.html"); //go to the login page
   const username = page.getByLabel("Username");
-  username.click(); //select username
+  await username.click(); //select username
   await username.fill("Dog"); //fill the input
   await expect(username).toHaveValue("Dog"); //checking the value matches what we expect
-  const password = page.getByPlaceholder("Enter password").click(); //select password
+  const password = page.getByPlaceholder("Enter password");
+  await password.click(); //select password
   await password.fill("123"); //fill the input
   await expect(password).toHaveValue("123"); //checking the value matches what we expect
-  const loginButton = page.getByRole("button", { name: "Login" }).click(); //click the submit button to login
-  await page.goto("http://127.0.0.1:5501/public-danny/index.html");
+  await page.getByRole("button", { name: "Login" }).click(); //click the submit button to login
+  await page.getByRole("button", { name: "continue" }).click();
   await page
     .getByRole("link", {
       name: "RESOURCE LIBRARY A library of resources to support your learning illustration of a man with big ideas",
@@ -22,33 +23,32 @@ test("Click add new", async ({ page }) => {
       name: "BACK-END JS, Databases, APIs and everything in between. illustration of a graph",
     })
     .click();
-  await page.goto("http://127.0.0.1:5501/public-danny/back-end.html");
   await page.locator("#submit-button").click();
 
-  const title = await page.getByPlaceholder("Enter a title...").click();
+  const title = page.getByPlaceholder("Enter a title...");
   await title.fill("Javascript");
   await expect(title).toHaveValue("Javascript");
 
-  const description = await page
-    .getByPlaceholder("Provide a link to your resource...")
-    .click();
+  const description = page.getByPlaceholder(
+    "Provide a link to your resource..."
+  );
+  // await description.click();
   await description.fill("https://www.youtube.com/watch?v=1A6SrPwmGpg");
   await expect(description).toHaveValue(
     "https://www.youtube.com/watch?v=1A6SrPwmGpg"
   );
 
-  const image = await page
-    .getByPlaceholder("Add an accompanying image...")
-    .click();
+  const image = page.getByPlaceholder("Add an accompanying image...");
+  // await image.click();
   await image.fill(
     "https://upload.wikimedia.org/wikipedia/commons/2/24/Circle-icons-image.svg"
   );
   await expect(image).toHaveValue(
     "https://upload.wikimedia.org/wikipedia/commons/2/24/Circle-icons-image.svg"
   );
-  const resourceLink = await page
-    .getByPlaceholder("Provide a link to your resource...")
-    .click();
+  const resourceLink = page.getByPlaceholder(
+    "Provide a link to your resource..."
+  );
   await resourceLink.fill("You Suck At Accessibility (But You Don't Have To)");
   await expect(resourceLink).toHaveValue(
     "You Suck At Accessibility (But You Don't Have To)"
