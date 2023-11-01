b381b900d4b0d414c41fa4a70a6ca1ecd14f57b0
Danny Ryan <dannykryan@gmail.com>
2023-11-01 16:59:45 +0000

    updated PORT in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import { app } from "./app.js";
 
-const PORT = process.env.PORT ?? 7001;
+const PORT = process.env.PORT ?? 10000;
 const HOST = process.env.HOST ?? "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
