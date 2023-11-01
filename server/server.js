5bd67f925b16bd338a551d1b3fb03ba9d293a1b6
Danny Ryan <dannykryan@gmail.com>
2023-11-01 16:51:20 +0000

    added HOST to server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import { app } from "./app.js";
 
-const PORT = process.env.PORT ?? 60006;
+const PORT = process.env.PORT ?? 7001;
+const HOST = process.env.HOST ?? "0.0.0.0";
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Server listening on port ${PORT}`);
 });
