e3ffc920c94001772ca0164524484ad26743406a
Danny Ryan <dannykryan@gmail.com>
2023-11-01 21:40:21 +0000

    added static line to app.js

--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use(cors());
 
 //express your routes and path
+app.use(express.static("public"));
 app.use("/frontend", frontendRoute);
 app.use("/backend", backendRoute);
 app.use("/funstuff", funstuffRoute);
